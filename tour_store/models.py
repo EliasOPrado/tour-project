7ff890475c406179b43c743c3b0f1160500c3b10
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-02-25 22:32:58 +0000

    fixed model

--- tour_store/models.py
+++ tour_store/models.py
@@ -5,13 +5,13 @@ from django.db import models
 
 class tour_destinations(models.Model):
     """This is a model to create destinations by the owner. """
-    author = models.CharField(max_length=200 unique=True)
+    author = models.CharField(max_length=200, unique=True)
     tour_title = models.CharField(max_length=250)
-    description = models.CharField(blank=True)
-    image = models.ImageField(upload_to='product', blank=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='tour_images', blank=True)
     location = models.CharField(max_length=250)
-    booking_start_date = models.DateField(u"Begin of the retreat", help_text="Begin of the retreat")
-    booking_end_date = models.DateField(u"End of the retreat", help_text="End of the retreat")
+    booking_start_date = models.DateField()
+    booking_end_date = models.DateField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
