c412cd11687ecceefbcb66fd364851304042139f
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-02-29 16:27:38 +0000

    Added product quantity to cart on navbar

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.view_cart, name="view_cart"),
     path('add/<int:id>/', views.add_to_cart, name="add_to_cart"),
     path('adjust/<int:id>/', views.adjust_cart, name="adjust_cart"),
-
 ]
--- cart/views.py
+++ cart/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect, reverse
 
 def view_cart(request):
     """A view that renders the cart contents page """
-    return render(request, cart.html)
+    return render(request, 'cart.html')
 
 def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
