01341b0d29748664e807d44e67b297b5e9c8d396
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-03-13 15:23:14 +0000

    added pagination to destination.html / tour_store.views.destinations

--- tour_store/views.py
+++ tour_store/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Destinations
 
 # Create your views here.
 def main_view(request):
     return render(request, 'main.html')
 
+
 def destinations(request):
-    destinations = Destinations.objects.all()
+    """
+    This function should run the pagination
+    """
+    destination = Destinations.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(destination, 1)
+    try:
+        destinations = paginator.page(page)
+
+    except PageNotAnInteger:
+        destinations = paginator.page(1)
+
+    except EmptyPage:
+        destinations = paginator.page(paginator.num_pages)
+
     return render(request, 'destinations.html', {'destinations': destinations})
 
 def destination_details(request, id):
