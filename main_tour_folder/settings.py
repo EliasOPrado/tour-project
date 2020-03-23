6e64b8ef591112230a3d6ae8d7f863bfce100d18
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-03-23 19:36:33 +0000

    removed config from wsgi file

--- main_tour_folder/settings.py
+++ main_tour_folder/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['tour-application.herokuapp.com']
+ALLOWED_HOSTS = ['tour-application.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
