8abe3a1668b7c54e0de0f059d0183b9a1bef4a4b
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-03-23 18:50:53 +0000

    added changes in settings to deploy

--- main_tour_folder/settings.py
+++ main_tour_folder/settings.py
@@ -96,15 +96,18 @@ Performing system checks...
 """
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    print("Postgres URL not found, using sqlite instead")
-    DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
 }
+else:
+    print("Postgres URL not found, using sqlite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
