d23f5a1df0fc4b218fdb2b410fce36b04bc6c202
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-04-15 20:21:44 +0100

    changed title

--- main_tour_folder/staticfiles/js/style.f6d3bbdb05fd.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
-});
