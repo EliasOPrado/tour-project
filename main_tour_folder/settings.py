cb3ac1772b605b0367dbe91fddd5c99bacaa9598
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-04-25 14:25:38 +0100

    fixed identation to deploy

--- main_tour_folder/settings.py
+++ main_tour_folder/settings.py
@@ -96,9 +96,9 @@ WSGI_APPLICATION = 'main_tour_folder.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 #if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 # else:
 #     print("Postgres URL not found, using sqlite instead")
 #     DATABASES = {
