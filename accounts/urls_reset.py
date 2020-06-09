1cfebc422cd9092e50b822848616cdfa5cd00948
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-06-09 19:26:19 +0100

    Fixed path position

--- accounts/urls_reset.py
+++ accounts/urls_reset.py
@@ -9,14 +9,8 @@ The registration folder with the password reset templates are stored in the main
 """
 urlpatterns = [
     path('',
-         auth_views.PasswordResetView.as_view(),
-         name='password_reset'),
-    path('password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(),
-         name='password_reset_done'),
-    path("<uidb64>/<token>", auth_views.PasswordResetConfirmView.as_view(),
-         name="password_reset_confirm"),
-    path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(),
-         name='password_reset_complete'),
+         auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path("<uidb64>/<token>", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
