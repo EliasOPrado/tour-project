3f5a13bd8733b81e5a30dfcc3958aa0045ce2d78
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-06-13 22:30:25 +0100

    added styles to login and register templates

---
+++ main_tour_folder/staticfiles/js/style.f6d3bbdb05fd.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
