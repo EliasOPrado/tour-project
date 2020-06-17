bbdcbe50df6e99e1b0661f461ca367f5da6bca18
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-06-17 02:17:37 +0100

    added conditional for empty query in search functionality

--- search/views.py
+++ search/views.py
@@ -3,6 +3,7 @@ from tour_store.models import Destinations
 # Create your views here.
 
 def do_search(request):
-    # 'q' is the name in the form that will be get// tour_title is the name that will be searched
-    destinations = Destinations.objects.filter(location__icontains=request.GET['q'])
-    return render(request, 'destinations.html', {'destinations': destinations})
+    if request.method == "GET":
+        # 'q' is the name in the form that will be get// tour_title is the name that will be searched
+        destinations = Destinations.objects.filter(location__icontains=request.GET['q'])
+        return render(request, 'destinations.html', {'destinations': destinations})
