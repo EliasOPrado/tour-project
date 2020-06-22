952cf47db18854c58ac4e26d74a4ebb79b6c7ac8
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-06-22 21:29:24 +0100

    added form tests

--- tour_store/tests/test_forms.py
+++ tour_store/tests/test_forms.py
@@ -21,9 +21,17 @@ class TestMForms(TestCase):
 
     def test_contact_form(self):
         #test if the form is valid
-        form = CommentForm(data={
+        form = ContactForm(data={
             'name': 'John',
             'email': 'john@email.com',
-            'comment': 'this is a comment.'
+            'subject': 'this is a title.',
+            'message': 'this is a message'
         })
         self.assertTrue(form.is_valid())
+
+    def test_contact_form_no_data(self):
+        #test if the form can validate Empty
+        #test if the form sends an error messaage for each field
+        form = ContactForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors),4)
