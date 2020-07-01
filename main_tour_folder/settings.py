d7cfe667fd2576cdcd55ceec0b35afea8f8be3e2
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-07-01 17:51:18 +0100

    removed local db to check gmail password reset issue

--- main_tour_folder/settings.py
+++ main_tour_folder/settings.py
@@ -94,18 +94,18 @@ WSGI_APPLICATION = 'main_tour_folder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    print("Postgres URL not found, using sqlite instead")
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            }
-        }
+# if 'DATABASE_URL' in os.environ:
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
+# else:
+#     print("Postgres URL not found, using sqlite instead")
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#             }
+#         }
 
 
 
