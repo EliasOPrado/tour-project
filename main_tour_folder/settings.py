121fb6056bc5b90fd4b39f6328512a886a555cc2
EliasOPrado <eliaspradoprofessional@outlook.com>
2024-09-15 20:05:25 -0300

    fixed configuration

--- main_tour_folder/settings.py
+++ main_tour_folder/settings.py
@@ -11,12 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
-# To import the env.py secret_keys
-if os.path.exists('env.py'):
-    import env
+from dotenv import load_dotenv
 
 import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,9 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY="DEJNSOIADiasideijdaiedaidjeie99"
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 """
