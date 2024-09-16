20f9503ab67d802df32ef7ff2244b600569d7036
EliasOPrado <eliaspradoprofessional@outlook.com>
2024-09-15 21:29:12 -0300

    changed base directory

--- main_tour_folder/settings.py
+++ main_tour_folder/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 
 """ Main  files config """
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "/media/"
